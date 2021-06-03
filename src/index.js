02d7b600a2ff3a670d0ce5996e34768f2142ed38
Dmitrii <heartnesnev@gmail.com>
2021-06-03 17:42:46 +0300

    feat: ts/react inline

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import {config} from './modules/config'
 import AppService from "./modules/app.service";
-import './modules/header.component'
 import './css/index.css'
+import './less/index.less'
+// import './scss/index.scss'
+import React from "react";
+import {render} from "react-dom"
+import App from "./App";
+import './modules/ts.module'
 
 console.log('Config key:', config.key)
 
 const service = new AppService('Hello world!')
 service.log()
+
+render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HTMLPlugin = require('html-webpack-plugin')
 const MiniCss = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 
 module.exports = {
@@ -24,23 +25,53 @@ module.exports = {
         ],
         minimize: true,
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port:4200,
+    },
     //! plugins
     plugins: [
+        new FaviconsWebpackPlugin({
+            logo: './src/logo/logo.ico',
+        }),
         new HTMLPlugin({
             filename: "index.html",
             template: "./src/index.html"
         }),
         new MiniCss({
             filename: 'style.css'
-        })
+        }),
 
     ],
-    //! module and loard
+    resolve: {
+        extensions: ['.js', ".ts"]
+    },
+    //! module and load
     module: {
         rules: [
+            //css loader
             {
-                test: /.s?css$/,
-                use: [MiniCss.loader, 'css-loader'] ,// 'sass-loader']
+                test: /.css$/,
+                use: [MiniCss.loader, 'css-loader'] ,
+            },
+            {
+                test: /.less$/,
+                use: [MiniCss.loader, 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.scss$/i,
+                use: [ MiniCss.loader, 'css-loader', 'sass-loader']
+            },
+            //babel
+            {
+                test: /\.(js|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
